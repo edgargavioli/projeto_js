2bad81d46785e5167eae5da1925aa69924958303
edgar <edgargavioli14@gmail.com>
2023-11-06 19:57:05 -0300

    arrumando bugs

--- schemas/coordenacao.js
+++ schemas/coordenacao.js
@@ -16,7 +16,7 @@ export default {
                 if(!file.asset){
                     return 'Nenhuma imagem selecionada'
                 }
-                if(Image.asset){
+                if(file.asset){
                     const {width, height} = Image.asset.metadata.dimensions
                     if(width !== 360 || height !== 360 && Image.asset.size > 10000){
                         return 'Tamanho de imagem inválido, 360x360px é o tamanho ideal'
