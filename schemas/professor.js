4a08ad013c14d96c5defc39a62df1ccd2cd92ec0
EED-Edgar <edgargavioli@outlook.com>
2023-11-20 18:53:43 -0300

    final modification

--- schemas/professor.js
+++ schemas/professor.js
@@ -12,18 +12,6 @@ export default {
             name: 'imagem_professor',
             type: 'image',
             title: 'Imagem do professor',
-            validation: Rule => Rule.custom(file =>{
-                if(!file.asset){
-                    return 'Nenhuma imagem selecionada'
-                }
-                if(file.asset){
-                    const {width, height} = Image.asset.metadata.dimensions
-                    if(width !== 360 || height !== 360 && Image.asset.size > 10000){
-                        return 'Tamanho de imagem inválido, 360x360px é o tamanho ideal'
-                    }
-                }
-                return true
-            })
         },
         {
             name: 'disciplina',
