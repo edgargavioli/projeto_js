98d60b354030ef33e275633541715615cd6451fc
edgar <edgargavioli14@gmail.com>
2023-11-20 20:43:34 -0300

    the last

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ import professor from './professor'
 import cursos_disponivies from './cursos'
 import noticias from './noticias'
 import coordenacao from './coordenacao'
-import avaliacoes from './avaliacoes'
+import avaliacao from './avaliacao'
 import perguntas from './perguntas_frequentes'
 
-export const schemaTypes = [professor, cursos_disponivies, noticias, coordenacao, avaliacoes, perguntas]
+export const schemaTypes = [professor, cursos_disponivies, noticias, coordenacao, avaliacao, perguntas]
